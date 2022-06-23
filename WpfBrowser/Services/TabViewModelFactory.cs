9969882d66c02440c3d81fac740f19d39439e52c
Nicolai Henriksen <nicolai.h80@gmail.com>
2022-06-23 14:02:35 +0200

    Initial commit - runtime data
    Outlines some of the struggles I am facing when needing to inject runtime parameters into types/services that are created/owned by the DI container.

---
+++ WpfBrowser/Services/TabViewModelFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.Messaging;
+using WpfBrowser.ViewModels;
+
+namespace WpfBrowser.Services;
+
+public interface ITabViewModelFactory
+{
+    TabViewModel Create(string tabName);
+}
+
+internal class TabViewModelFactory : ITabViewModelFactory
+{
+    protected readonly IServiceProvider ServiceProvider;
+
+    public TabViewModelFactory(IServiceProvider serviceProvider)
+    {
+        ServiceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+    }
+
+    public virtual TabViewModel Create(string tabName)
+    {
+        return new TabViewModel(ServiceProvider.GetRequiredService<IMessenger>(), tabName);     // Ugh!!! Needs to be updated if ctor signature changes
+    }
+}
+
+internal class ContainerSupportedTabViewModelFactory : TabViewModelFactory
+{
+    public ContainerSupportedTabViewModelFactory(IServiceProvider serviceProvider) : base(serviceProvider)
+    { }
+
+    public override TabViewModel Create(string tabName)
+    {
+        var viewModel = ServiceProvider.GetRequiredService<TabViewModel>();
+        viewModel.TabName = tabName;    // !!! Code-smell !!! - Temporal coupling
+        return viewModel;
+    }
+}
+
+internal class ScopedContainerSupportedTabViewModelFactory : TabViewModelFactory
+{
+    public ScopedContainerSupportedTabViewModelFactory(IServiceProvider serviceProvider) : base(serviceProvider)
+    { }
+
+    public override TabViewModel Create(string tabName)
+    {
+        var scope = ServiceProvider.CreateScope();  // How to dispose this scope when the tab is closed??
+        var viewModel = scope.ServiceProvider.GetRequiredService<ScopedTabViewModel>();
+        viewModel.TabName = tabName;    // !!! Code-smell !!! - Temporal coupling
+        viewModel.Scope = scope;        // !!! Code-smell !!! - Temporal coupling
+        return viewModel;
+    }
+}
---
+++ WpfBrowser/Views/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using MahApps.Metro.Controls;
+using WpfBrowser.ViewModels;
+
+namespace WpfBrowser.Views;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : MetroWindow
+{
+    public MainWindow(MainWindowViewModel viewModel)
+    {
+        DataContext = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
+
+        InitializeComponent();
+    }
+
+    private void LaunchGitHubSite(object sender, System.Windows.RoutedEventArgs e)
+    {
+        Process.Start("explorer", "https://github.com/nicolaihenriksen/WpfBrowser");
+    }
+}
